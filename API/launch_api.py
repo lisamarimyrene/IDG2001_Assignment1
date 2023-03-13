7f85c4dcbd6ba7f7896b3f5c7356b394290f66d7
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-13 22:40:05 +0100

    database changes

--- API/launch_api.py
+++ API/launch_api.py
@@ -1,22 +1,10 @@
 # Flask, render_template, request
 from flask import Flask, render_template, request
-# Flask with mongo db
-from flask_mongoengine import MongoEngine
-
 import os
 
 # Set the flask app
 app = Flask(__name__)
 
-# Connect to mongo database 
-database_name = "vCardDB"
-DB_URI = "mongodb+srv://lisamarimyrene:mongodbpassword@cluster0.mub535i.mongodb.net/vCardDB?retryWrites=true&w=majority"
-app.config["MONGODB_HOST"] = DB_URI
-
-# Set the database
-db = MongoEngine()
-# Put the app inside the database
-db.init_app(app)
 
 # Homepage, find the form
 @app.route('/')
