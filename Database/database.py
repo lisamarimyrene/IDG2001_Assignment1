a5bfa9b03561411a2a66b34c9199fd7b9daa16bd
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-13 22:40:08 +0100

    database changes

--- Database/database.py
+++ Database/database.py
@@ -11,3 +11,22 @@ client = MongoClient(os.getenv("mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@
 
 db = client.get_default_database()
 
+
+''' Found this method also, using MongoEngine instead of MongoClient, don't know what is best
+
+https://www.mongodb.com/compatibility/mongoengine-pymongo 
+
+# Flask with mongo db
+from flask_mongoengine import MongoEngine
+
+# Connect to mongo database 
+database_name = "vCardDB"
+DB_URI = "mongodb+srv://lisamarimyrene:mongodbpassword@cluster0.mub535i.mongodb.net/vCardDB?retryWrites=true&w=majority"
+app.config["MONGODB_HOST"] = DB_URI
+
+# Set the database
+db = MongoEngine()
+# Put the app inside the database
+db.init_app(app)
+
+'''
