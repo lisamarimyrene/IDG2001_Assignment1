a832bee105770dec79e22d1296f8a4e372720572
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-19 16:51:24 +0100

    changed setup

--- Backend/database.py
+++ Backend/database.py
@@ -22,11 +22,33 @@ from flask_mongoengine import MongoEngine
 # Connect to mongo database 
 database_name = "vCardDB"
 DB_URI = "mongodb+srv://lisamarimyrene:mongodbpassword@cluster0.mub535i.mongodb.net/vCardDB?retryWrites=true&w=majority"
-app.config["MONGODB_HOST"] = DB_URI
+app.config["MONGODB_HOST"] = DB_URI 
 
 # Set the database
 db = MongoEngine()
 # Put the app inside the database
 db.init_app(app)
+
+'''
+
+'''
+
+from pymongo import MongoClient
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['user_shopping_list']
+  
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":   
+  
+   # Get the database
+   dbname = get_database()
 
 '''
